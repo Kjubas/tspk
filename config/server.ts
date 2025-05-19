c142c9e6a619aa1fa1940c9c594e8671148acb47
Kjubas <Nonne2@yandex.ru>
2025-05-19 17:49:28 +0300

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'tay7Pl3WIWmXiYmwXdQcTQ==',
+      'fbk2KcQl9kbEKmLcinmA7Q==',
+      '7OT1GlGxmb8XetotxNbhJA==',
+      'wOWFBDNDE0z2LfDFwRLE2w=='
+    ]),
   },
 });

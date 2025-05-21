c19fa13da29e7468f5b16b2ed7af5888f75369b2
Kjubas <Nonne2@yandex.ru>
2025-05-21 18:38:01 +0300

    Изменения middlewares js
    Тестовые

--- config/production/middlewares.js
+++ config/production/middlewares.js
@@ -1,10 +1,15 @@
-module.exports = {
-    settings: {
-      cors: {
+module.exports = [
+    'strapi::errors',
+    'strapi::security',
+    {
+      name: 'strapi::cors',
+      config: {
         origin: [
           'https://tspk-new.vercel.app',
           'https://tspk.onrender.com'
         ],
-      },
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        headers: ['Content-Type', 'Authorization']
+      }
     },
-  };
+  ];

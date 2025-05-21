d915cb487e63f2e694670ed2ee70c0de40ed6628
Kjubas <Nonne2@yandex.ru>
2025-05-21 18:19:14 +0300

    production и middlewwares.js
    Создание папки production и кастомного middlewwares.js для CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -10,19 +10,3 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
-module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', '*.amazonaws.com'],
-          'media-src': ["'self'", 'data:', 'blob:', '*.amazonaws.com'],
-        },
-      },
-    },
-  },
-  'strapi::cors',
-];
---
+++ config/production/middlewares.js
@@ -0,0 +1,10 @@
+module.exports = {
+    settings: {
+      cors: {
+        origin: [
+          'https://tspk-new.vercel.app',
+          'https://tspk.onrender.com'
+        ],
+      },
+    },
+  };
